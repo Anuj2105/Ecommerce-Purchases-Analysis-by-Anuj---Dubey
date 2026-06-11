4393500caa74784fc9fcfabd128d75d031abe160
ANUJ DUBEY <135577833+Anuj2105@users.noreply.github.com>
2026-06-11 11:06:00 +0530

    Update Ecommerce Purchases Analysis by Anuj - Dubey.py

--- Ecommerce Purchases Analysis by Anuj - Dubey.py
+++ Ecommerce Purchases Analysis by Anuj - Dubey.py
@@ -1,20 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# ___
-# Mr. Yogesh P Murumkar (965708095)
-# 
-# www.youtube.com/yogeshmurumkar
-# ___
 # # Ecommerce Purchases Exercise
 # 
-# In this Exercise you will be given some Fake Data about some purchases done through Amazon! Just go ahead and follow the directions and try your best to answer the questions and complete the tasks. Feel free to reference the solutions. Most of the tasks can be solved in different ways. For the most part, the questions get progressively harder.
-# 
-# Please excuse anything that doesn't make "Real-World" sense in the dataframe, all the data is fake and made-up.
-# 
-# Also note that all of these questions can be answered with one line of code.
-# ____
-# ** Import pandas and read in the Ecommerce Purchases csv file and set it to a DataFrame called ecom. **
+
 
 # In[1]:
 
